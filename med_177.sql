0446a5111c7c5bcc091312af5480b1978eb2c0bf
lingjie <lingjie@u.nus.edu>
2022-06-21 22:55:13 +0800

    21 Jun

---
+++ med_177.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+  RETURN (
+    SELECT
+        salary
+    FROM Employee
+    GROUP BY salary
+    ORDER BY salary DESC
+    LIMIT 1
+    OFFSET N
+  );
+END
